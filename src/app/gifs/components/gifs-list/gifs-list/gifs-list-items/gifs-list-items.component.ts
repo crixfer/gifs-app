b57834eb526ec7995ebaf96c6488918fe62732d0
crixfer <crixfer08@gmail.com>
2025-04-12 00:03:10 -0400

    work with list component

--- src/app/gifs/components/gifs-list/gifs-list/gifs-list-items/gifs-list-items.component.ts
+++ src/app/gifs/components/gifs-list/gifs-list/gifs-list-items/gifs-list-items.component.ts
@@ -1,23 +1,10 @@
-import { Component } from '@angular/core';
-
-const imageUrls: string[] = [
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg',
-];
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'gifs-list-items',
   imports: [],
   templateUrl: './gifs-list-items.component.html',
 })
-export class GifsListItemsComponent {}
+export class GifsListItemsComponent {
+  imageUrl = input.required<string>();
+}
--- src/app/gifs/components/gifs-list/gifs-list/gifs-list.component.ts
+++ src/app/gifs/components/gifs-list/gifs-list/gifs-list.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { GifsListItemsComponent } from './gifs-list-items/gifs-list-items.component';
+import { imageUrls } from 'src/app/gifs/pages/trending-page/trending-page.component';
 
 @Component({
   selector: 'gifs-list',
   imports: [GifsListItemsComponent],
   templateUrl: './gifs-list.component.html',
 })
-export class GifsListComponent {}
+export class GifsListComponent {
+  gifs = input.required<string[]>();
+}
