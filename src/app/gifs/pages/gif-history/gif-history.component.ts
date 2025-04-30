6d96b82c4ec3d3e150ecd05caedcf4feb75aaf5b
crixfer <crixfer08@gmail.com>
2025-04-30 12:39:52 -0400

    cambio de foto y nombre en side-menu-header

--- src/app/gifs/pages/gif-history/gif-history.component.ts
+++ src/app/gifs/pages/gif-history/gif-history.component.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
+
 import { GifService } from '../../services/gifs.service';
 import { GifsListComponent } from '../../components/gifs-list/gifs-list/gifs-list.component';
 
